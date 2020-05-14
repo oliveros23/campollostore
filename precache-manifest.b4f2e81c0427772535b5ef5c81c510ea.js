3c035b4e76789a445ce340b465d985f6faa90be1
oliveros23 <65323252+oliveros23@users.noreply.github.com>
2020-05-13 21:25:37 -0500

    Add files via upload

---
+++ precache-manifest.b4f2e81c0427772535b5ef5c81c510ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3488634ee412943347d7c698d3aea7fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b65bf1127e0b57821e12",
+    "url": "/static/css/2.fc3ea6fc.chunk.css"
+  },
+  {
+    "revision": "f7c40092b535ff6d45a9",
+    "url": "/static/css/main.7013d9c1.chunk.css"
+  },
+  {
+    "revision": "b65bf1127e0b57821e12",
+    "url": "/static/js/2.1708cb70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1708cb70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c40092b535ff6d45a9",
+    "url": "/static/js/main.cfe17a77.chunk.js"
+  },
+  {
+    "revision": "3fab58ee6f9a56b2c9b1",
+    "url": "/static/js/runtime-main.81dd1bbb.js"
+  }
+]);
